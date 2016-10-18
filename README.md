16d22801d3089ad87005dd13b50b01dc00667052
joshdrink <me@joshbeveridge.com>
2016-10-17 21:03:31 -0300

    Untracked node_modules

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# joshdrink.github.io
-Jekyllin' it up.
+# Clone
+
+A front-end framework that uses NPM and PostCSS.
+
+### Getting Started
+
+1. Clone Clone.
+2. `cd clone/assets`
+3. `npm install`
+4. `npm run clone`
+5. Profit.
+
+### PostCSS
+
+Clone includes the following processors by default:
+* postcss-import
+* postcss-mixins
+* postcss-nested
+* autoprefixer
+* postcss-simple-vars
+* postcss-color-function
+* cssnano
+
+#### Compiling PostCSS
+
+1. `cd clone/assets`
+2. `npm run watch`
+3. CSS away!
+
+#### Using the Grid
+Clone includes a responsive flexbox-based grid system to help with your layout stuggles.
+
+The structure works as follows:
+```
+<div class="flex-grid">
+    <div class="box small-1of2 med-1of3 lg-1of5">
+        <!-- Content -->
+    </div>
+</div>
+```
+
+The `.flex-grid` class is required on the grid itself, while grid items are required to have the `.box` class. `.small-XofX`, `.med-XofX`, and `.lg-XofX` are used to address column width based on viewports:
+* small refers to mobile devices
+* medium refers to portrait tablet devices
+* large refers to landscape tablet devices
+
+The `XofX` portion of the class can span up to 12 columns (e.g. 1of2, 3of12).
+
+To create a full width item, you can use the `.full`, `.small/med/lg-1of1`, or `.small/med/lg-full` classes.
+
+#### Editing PostCSS Processors
+
+1. Open `package.json`
+2. Add your NPM Package to the `devDependencies` list.
+3. Add your NPM Package to the `post` script using `-u packageName`. **Verify the order of your packages to avoid complications. Most packages should be included after `postcss-nested` and before `cssnano`.**
+4. `cd clone/assets`
+5. `npm install`
+6. `npm run watch`
+7. Profit.
+
+### Editing Vendor Dependencies
+
+Clone includes the following dependencies by default:
+* Font Awesome
+* Ionicons
+* jQuery
+* Match Media
+* Modernizr
+* Normalize
+
+#### Adding/Removing Dependencies
+
+1. Open `bower.json`
+2. Add your Bower Package to the `dependencies` list.
+3. Open `package.json`
+4. Inside `scripts`, write a script that installs your Bower Package, and copies out the resulting asset into vendor. Finally, add your new script to the `clone` script. See `package.json` for examples.
+5. `cd clone/assets`
+6. `npm run clone`
+7. Include your `assets/vendor/file_name.ext` resource in your HTML.
+8. Profit.
+
+**Please note that Clone uses `latest` in `bower.json` to pull down the most recent version of all default dependencies. If you need a specific version, replace `latest` with the correct version number before running `npm run clone`.**
--- assets/js/app.js
+++ assets/js/app.js
@@ -5,181 +5,47 @@
     // Authored by Josh Beveridge and Justin Bellefontaine
 
 // =============================================================================
+
 (function($) {
 
     $(document).ready(function() {
 
-        jQuery.extend(jQuery.expr[':'], {
-            focus: "a == document.activeElement"
+        var $root = $('html, body');
+
+        // User Agent Data Attributes ==========================================
+        var ua = navigator.userAgent;
+        ua = ua.toString();
+        $('body').attr('id', ua);
+
+        // Disabled Button Clicks ==============================================
+        $('.disabled').on('click', function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
         });
 
         // Smooth Scrolling ====================================================
-        $("a[href*='#']:not([href='#'])").on('click',function() {
+        $('a[href*="#"]:not([href="#"])').on('click',function() {
 
-            if(!$(this).parent().hasClass('accordion-navigation')) {
+            $root.animate({
+                scrollTop: $( $(this).attr('href') ).offset().top
+            }, 500); // change the duration of your animation in ms
 
-                if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+            return false;
 
-                    var target = $(this.hash);
+         });
 
-                    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
-                    if (target.length) {
-                        $('html,body').animate({
-                            scrollTop: target.offset().top - $('.search-cta').outerHeight()
-                        }, 650); // Duration
-                        return false;
-                    }
-
-                }
-
-            }
-
-        });
-
-        // Mobile Navigation Handler ===========================================
-        $('nav.mobile .open').on('click', function(e) {
-            var navListHeight = $('.nav-list').outerHeight();
-            $('nav.mobile .wrapper').css('max-height', navListHeight + 'px');
-        });
-
-        $('nav.mobile .close').on('click', function(e) {
-            $('nav.mobile .wrapper').css('max-height', 0);
-            e.preventDefault();
-        });
-
-        // Desktop Navigation Handler ==========================================
-		var navHeight = $('header').outerHeight();
-
-        if (matchMedia('screen and (max-width: 1023px)').matches) {
-
-            $('.latest-posts').css('padding-bottom', navHeight + 'px');
-
-        }
-
-        if (matchMedia('screen and (min-width: 1024px)').matches) {
-
-            // $('.content').css('margin-top', navHeight + 'px');
-
-            $(function() {
-
-    			var prevScroll = $(document).scrollTop();
-
-    			$(window).scroll(function() {
-    				scrollPosition = $(document).scrollTop();
-    				if(scrollPosition > navHeight) {
-    					var newScroll = $(document).scrollTop();
-    			        if(newScroll < prevScroll) {
-    			            $('header').removeClass('active');
-    			        } else {
-    			            $('header').addClass('active');
-    			        }
-    			        prevScroll = newScroll;
-    				}
-    		    });
-
-    		});
-
-        }
-
-        // Mailchimp Labels ====================================================
-        $(".mailchimp input").focus(function(e) {
-            $(this).parent().find("label").addClass("active");
-        });
-
-        $(".mailchimp input").blur(function(e) {
-
-            if(!$(this).val()) {
-                $(this).parent().find("label").removeClass('active');
-            }
-
-        });
-
-        // Intro Labels ========================================================
-        $(".intro input").focus(function(e) {
-            $(this).parent().find("label").addClass("active");
-        });
-
-        $(".intro input").blur(function(e) {
-
-            if(!$(this).val()) {
-                $(this).parent().find("label").removeClass('active');
-            }
-
-        });
-
-        // Post Filters ========================================================
-        $('#writingFilter').keyup(function(e) {
-
-            var input = this.value.toLowerCase()
-
-            $('.main.archive .article-list dd h3').each(function () {
-                var text  = $(this).text().toLowerCase();
-
-                if(text.indexOf(input) >= 0) {
-                    $(this).parents('.article-list dd').hide();
-                    $(this).parents('dd').show();
-                }
-                else {
-                    $(this).parents('.article-list dd').hide();
-                }
-
-            });
-
-            if($('.article-list dd.summary:visible').length) {
-                $('.no-result').hide();
+        // Data Link Handler ===================================================
+        $('[data-link-handler]').on('click',function(e){
+            if($('[data-link-target="'+$(this).attr('data-link-handler')+'"]').hasClass('active')) {
+                $('[data-link-target="'+$(this).attr('data-link-handler')+'"]').removeClass('active');
             }
             else {
-                $('.no-result').show();
+                $('[data-link-target="'+$(this).attr('data-link-handler')+'"]').addClass('active');
             }
-
             e.preventDefault();
-
         });
 
     });
-
-    window.onload = function () {
-
-        // Post Sidebar Timer Handler ==========================================
-        if($(".main").hasClass("post")) {
-
-            if (matchMedia("screen and (min-width: 64em)").matches) {
-
-                var postTime = $(".sidebar .length p span").text();
-                var postLength = $(document).height();
-                var windowHeight = $(window).height();
-
-                console.log(postTime);
-                console.log(postLength);
-                console.log(windowHeight);
-
-                $(window).resize(function() {
-                    windowHeight = $(window).height();
-                });
-
-                $(window).scroll(function() {
-                    var userPosition = $(document).scrollTop();
-                    var remainingPost = (userPosition/(postLength - windowHeight));
-                    var timeLeft = Math.ceil((postTime - (postTime*remainingPost)));
-
-                    $(".sidebar .length p span").text(timeLeft);
-                    if(timeLeft == 1) {
-                        $(".sidebar .length p").text(timeLeft + " Minute Left");
-                    }
-                    if(timeLeft == 0) {
-                        $(".sidebar .length p").text("All Done!");
-                    }
-                    if(timeLeft > 1) {
-                        $(".sidebar .length p").text(timeLeft + " Minutes Left");
-                    }
-
-                });
-
-            }
-
-        }
-
-    }
 
 })(jQuery);
